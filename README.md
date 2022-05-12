70fc67802b6fe76c329a657107ce17afc6e4f2be
Clint Chester <clint.chester@synergy.net.au>
2022-05-12 09:10:30 +0000

    Creation of Bicep Linter

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# bicep-lint-action
-GitHub Action to lint Bicep - https://github.com/Azure/bicep
+# Bicep Lint Action
+GitHub Action to lint [Bicep](https://github.com/Azure/bicep). This will show the linting messages in both the pull requests and the actions workflow runs. You are also able to set your own [Bicep linter configuration](https://docs.microsoft.com/en-us/azure/azure-resource-manager/bicep/bicep-config-linter) which this action will respect.
+
+## Input Configuration Options
+analyse-all-files
+- **Description**: Used to determine whether you just want to analyse the files changed or the whole repository. When set to false on a pull request event, it will compare between the current and target branch. When set to false on a push event, it will compare the changes between two commits.
+- **Required**: false
+- **Default**: 'false'
+
+## Example Workflow File
+```
+name: Linter
+on:
+  pull_request:
+  workflow_dispatch:
+
+jobs:
+  bicep-linter:
+    name: Bicep
+    runs-on: ubuntu-latest
+    steps:
+    - name: Checkout Repository
+      uses: actions/checkout@v3
+      with:
+        # Incremental diffs require fetch depth to be at 0 to grab the target branch
+        fetch-depth: '0'
+    - name: Run Bicep Linter
+      uses: synergy-au/bicep-lint-action@v1
+      with:
+        analyse-all-files: 'true'
+```
---
+++ bicep-lint.sh
@@ -0,0 +1,48 @@
+# shellcheck shell=sh
+
+## Download Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Now either run the full analysis or files changed based on the settings defined
+if [ "$ANALYSE_ALL_FILES" == 'true' ]; then
+    while read -r file; do
+        az bicep build --file "$file" 2>> errors.txt
+    done <<< "$(find . -type f -name '*.bicep')"
+else
+    if [ "$ACTION_EVENT_NAME" == 'pull_request' ]; then
+        while read -r file; do
+            az bicep build --file "$file" 2>> errors.txt
+        done <<< "$(git diff --name-only --diff-filter=d origin/"$CURRENT_CODE"..origin/"${CHANGED_CODE#"refs/heads/"}")"
+    else
+        while read -r file; do
+            az bicep build --file "$file" 2>> errors.txt
+        done <<< "$(git diff --name-only --diff-filter=d "$CURRENT_CODE".."$CHANGED_CODE")"
+    fi
+fi
+
+## Clean empty lines
+sed -i '/^[[:blank:]]*$/ d' errors.txt
+
+## Clean the notifications that a linter configuration was found
+sed -i '/Linter Configuration/d' errors.txt
+
+## Loop through each line identified in linter and set a GitHub message
+while read -r message; do
+    SEVERITY="$(echo "$message" | grep -E ' Info | Warning | Error ' -o | sed 's/ //g')"
+    FILENAME="$(sed "s#.*$WORKSPACE\([^(]*\).*#\1#" <<<"$message")"
+    LINE="$(sed "s#.*(\([^,]*\).*#\1#" <<<"$message")"
+    COLUMN="$(sed "s#.*,\([^)]*\).*#\1#" <<<"$message")"
+    REMOVE_TITLE_START="${message#* : }"
+    REMOVE_TITLE_END="${REMOVE_TITLE_START%%: *}"
+    TITLE="${REMOVE_TITLE_END#*"$SEVERITY"}"
+    MESSAGE="${message##*: }"
+
+    ## Sets GitHub Notifications based on severity set
+    if [ "$SEVERITY" == 'Info' ]; then
+        echo "::notice file=$FILENAME,line=$LINE,col=$COLUMN,title=$TITLE::$MESSAGE"
+    elif [ "$SEVERITY" == 'Error' ]; then
+        echo "::error file=$FILENAME,line=$LINE,col=$COLUMN,title=$TITLE::$MESSAGE"
+    else
+        echo "::warning file=$FILENAME,line=$LINE,col=$COLUMN,title=$TITLE::$MESSAGE"
+    fi
+done < errors.txt
